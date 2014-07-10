128e83cc9694b5bc0ae63472a5b6e4f8f34bc228
Chris Matthieu <chris@matthieu.us>
2014-07-09 17:03:25 -0700

    cleaned up js and zoomed in 1 more level

--- public/js/site.js
+++ public/js/site.js
@@ -15,13 +15,6 @@ conn.on('ready', function(data){
   console.log('UUID AUTHENTICATED!');
   console.log(data);
 
-  // // Subscribe to SkyNet.im messages and events
-  // conn.emit('subscribe', {
-  //   "uuid": "4c750541-0608-11e4-ab3f-b15969385230"
-  // }, function (data) {
-  //   console.log(data);
-  // });
-
   conn.subscribe({
     "uuid": "4c750541-0608-11e4-ab3f-b15969385230"
   }, function (data) {
@@ -46,57 +39,12 @@ conn.on('ready', function(data){
 });
 
 
-
-
-
-// var skynetConfig = {
-//   "uuid": "c33e14c0-fd55-11e3-a290-ef9910e207d9",
-//   "token": "0avr9lpll33w0cnmirsghi79omie8kt9",
-//   "protocol": "websocket",
-//   "server": "skynet.im", // optional (defaults to "http://skynet.im")
-//   "port": 80 // optional (defaults to 80)
-//   // "host": "127.0.0.1", // optional (defaults to "http://skynet.im")
-//   // "port": 3000 // optional (defaults to 80)
-// }
-
-// skynet.createConnection(skynetConfig, function (e, socket, device) {
-//   if (e) throw e
-
-//     console.log('connected to skynet');
-
-//   // Subscribe to SkyNet.im messages and events
-//   socket.emit('subscribe', {
-//     // "uuid": "530ec7a1-02f8-11e4-a1b9-fd8f2922fbb0"
-//     "uuid": "4c750541-0608-11e4-ab3f-b15969385230"
-//   }, function (data) {
-//     console.log(data);
-//   });
-
-//   socket.on('message', function(message){
-//     console.log('message received', message);
-//     console.log('IP address', message.payload.ipAddress);
-
-//     $.ajax({
-//       url: "/geo/" + message.payload.ipAddress,
-//       cache: true
-//     })
-//       .done(function( data ) {
-//         console.log('geo', data);
-//         formatData(data);
-//       });
-
-    
-//   });
-
-// });
-
-
 var map = L.mapbox.map('map', 'chrismatthieu.im763216', {
     tileLayer: {
         detectRetina: true
     }
 })
-    .setView([0, 0], 2);
+    .setView([0, 0], 3);
 
 function formatData(geo) {
     var marker = L.marker([geo[0], geo[1]], {
