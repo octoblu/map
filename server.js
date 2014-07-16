92db03a6fe673e9d3aa128bde07dd343f62927d6
Chris Matthieu <chris@matthieu.us>
2014-07-16 08:44:18 -0700

    removed dependency on geoip-lite

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 5000;
-var geoip = require('geoip-lite');
 
 app.use(express.static(__dirname + '/public'));
 
