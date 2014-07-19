f2f36193bde66f503786dce6819d09b1833c554b
Roy van de Water <dev+roy@integrumtech.com>
2014-07-19 11:49:03 -0700

    Move most functionality into named methods

--- public/js/site.js
+++ public/js/site.js
@@ -1,121 +1,125 @@
-var path = 'ws://54.191.102.104';
-var ws = new WebSocket(path);
+(function(){
+    var ws, map, formatData, formatLocation, formatPopupHTML,
+        addActivityToSidebar, showFromPopUp, showToPopUp, HIDE_DELAY;
 
-ws.onmessage = function(evt){
-    try{
-        var data = JSON.parse(evt.data);
+    // How long to show an icon, popup, line before it disappears (in ms)
+    HIDE_DELAY = 1000;
+
+    ws = new WebSocket('ws://54.191.102.104');
+
+    ws.onmessage = function(evt){
+        var data;
+        try{
+            data = JSON.parse(evt.data);
+        }
+        catch(ex){
+            console.log('JSONParseError:', evt.data);
+        }
+
         if(data.geo){
-
-            /* testing destination
-            data.toGeo = {"ll":[], "region":"", "city":"", "country":"" };
-            data.toGeo.ll[0] = 33.5;
-            data.toGeo.ll[1] = -112;
-            data.toGeo.region = "AZ";
-            data.toGeo.city = "Mesa";
-            data.toGeo.country = "US";
-            */
-
-
             formatData(data);
         }
         console.log('evt', data);
+    };
 
-    }
-    catch(ex){
-        console.log('erro on data', ex);
-    }
-};
-
-var map = L.mapbox.map('map', 'chrismatthieu.im763216', {
-    tileLayer: {
-        detectRetina: true
-    }
-})
-    .setView([0, 0], 3);
+    map = L.mapbox.map('map', 'chrismatthieu.im763216', {
+        tileLayer: {
+            detectRetina: true
+        }
+    }).setView([0, 0], 3);
 
 
-function formatData(data) {
-   var activity = '<span class="display-block small">' +
-                    '<span class="white inline">' + data.topic + '</span>' +
-                    '<span class="quiet inline"> @' + data.type + '</span>' +
-                '</span>' +
-                data.geo.city + ' ' +
-                data.geo.region + ' ' +
-                data.geo.country
-    $("#activityData").prepend(activity);
+    formatData = function(data) {
+        addActivityToSidebar(data);
+        showFromPopUp(data);
+        showToPopUp(data);
 
-    var marker = L.marker([data.geo.ll[0], data.geo.ll[1]], {
-        icon: L.icon({
-            iconUrl: 'http://octoblu-devices.s3.amazonaws.com/octoblupin25.png',
-            iconSize: ['15', '15'],
-            iconAnchor: ['12', '12']
-        })
-    });
-    var fromPopUp = L.popup();
-    fromPopUp.setLatLng(L.latLng(data.geo.ll[0], data.geo.ll[1]));
-    fromPopUp.setContent('<div class="pad1 round clip tile fl">' +
-            '<p class="small fr col9 white">' +
-            '<span class="display-block small">' +
-            '<span class="white inline">' + data.topic + '</span>' +
-            ' <span class="quiet inline"> @' + data.type + '</span>' +
-            '</span>' +
-            data.geo.city + ' ' +
-            data.geo.region + ' ' +
-            data.geo.country + ' ' +
-            '</p>' +
-            '</div>' + '');
+        $('.loading').fadeOut();
+    };
 
-    if (data.toGeo) {
-        var destMarker = L.marker([data.toGeo.ll[0], data.toGeo.ll[1]], {
-            icon: L.icon({
-                iconUrl: 'http://octoblu-devices.s3.amazonaws.com/octoblupin25.png',
-                iconSize: ['15', '15'],
-                iconAnchor: ['12', '12']
-            })
-        });
+    addActivityToSidebar = function(data) {
+        var activityHTML;
 
-        var destPopUp = L.popup();
-        destPopUp.setLatLng(L.latLng(data.toGeo.ll[0], data.toGeo.ll[1]));
-        destPopUp.setContent('<div class="pad1 round clip tile fl">' +
-            '<p class="small fr col9 white">' +
-            '<span class="display-block small">' +
-            '<span class="white inline">Dest:' + data.topic + '</span>' +
-            '<span class="quiet inline"> @' + data.type + '</span>' +
-            '</span>' +
-            data.toGeo.city + ' ' +
-            data.toGeo.region + ' ' +
-            data.toGeo.country + ' ' +
-            '</p>' +
-            '</div>' + '');
+        activityHTML =  '<span class="display-block small">' +
+                            '<span class="white inline">' + data.topic + '</span>' +
+                            '<span class="quiet inline"> @' + data.type + '</span>' +
+                        '</span>' + formatLocation(data.geo);
 
-        var fromTo = L.polyline([L.latLng(data.geo.ll[0], data.geo.ll[1]), L.latLng(data.toGeo.ll[0], data.toGeo.ll[1])], {color: 'blue'});
+        $("#activityData").prepend(activityHTML);
+    };
 
+    showFromPopUp = function(data) {
+        var marker, popup;
 
+        marker = buildMarker(data.geo);
 
-    }
+        popup = L.popup();
+        popup.setLatLng(L.latLng(data.geo.ll[0], data.geo.ll[1]));
+        popup.setContent(formatPopupHTML(data, data.geo, 'Source'));
 
-    map.addLayer(marker);
-    map.addLayer(fromPopUp);
+        map.addLayer(marker);
+        map.addLayer(popup);
 
-    setTimeout(function(){map.removeLayer(marker);}, 1000);
-    setTimeout(function(){map.removeLayer(fromPopUp);}, 1000);
+        setTimeout(function(){map.removeLayer(marker);}, HIDE_DELAY);
+        setTimeout(function(){map.removeLayer(popup);},  HIDE_DELAY);
+    };
 
-    if (data.toGeo) {
-        map.addLayer(fromTo);
+    showToPopUp = function(data) {
+        var marker, popup, line;
+        if (!data.toGeo) {
+            return;
+        }
+
+        marker = buildMarker(data.toGeo);
+
+        popup = L.popup();
+        popup.setLatLng(L.latLng(data.toGeo.ll[0], data.toGeo.ll[1]));
+        popup.setContent(formatPopupHTML(data, data.toGeo, 'Destination'));
+
+        var line = L.polyline([L.latLng(data.geo.ll[0], data.geo.ll[1]), L.latLng(data.toGeo.ll[0], data.toGeo.ll[1])], {color: 'blue'});
+
+        map.addLayer(line);
 
         /* Maybe there is a better way to focus the view,
          panInsideBounds will move the map from one place to another
          if there is high activity */
-        map.panInsideBounds(fromTo.getBounds());
+        map.panInsideBounds(line.getBounds());
 
-        map.addLayer(destMarker);
-        map.addLayer(destPopUp);
-        setTimeout(function(){map.removeLayer(fromTo);}, 1000);
-        setTimeout(function(){map.removeLayer(destMarker);}, 1000);
-        setTimeout(function(){map.removeLayer(destPopUp);}, 1000);
-
+        map.addLayer(marker);
+        map.addLayer(popup);
+        setTimeout(function(){map.removeLayer(line);},   HIDE_DELAY);
+        setTimeout(function(){map.removeLayer(marker);}, HIDE_DELAY);
+        setTimeout(function(){map.removeLayer(popup);},  HIDE_DELAY);
     }
 
+    buildMarker = function(geo) {
+        return L.marker([geo.ll[0], geo.ll[1]], {
+            icon: L.icon({
+                iconUrl: 'http://octoblu-devices.s3.amazonaws.com/octoblupin25.png',
+                iconSize: ['25', '25'],
+                iconAnchor: ['12', '12']
+            })
+        });
+    }
 
-    $('.loading').fadeOut();
-}
+    formatLocation = function(geo) {
+        return [
+           geo.city,
+           geo.region,
+           geo.country
+        ].join(' ');
+    };
+
+    formatPopupHTML = function(data, geo, info){
+        return '' +
+            '<div class="pad1 round clip tile fl">' +
+                '<p class="small fr col9 white">' +
+                    '<span class="display-block small">' +
+                        '<span class="white inline">' + info + ': ' + data.topic + '</span>' +
+                        '<span class="quiet inline"> @' + data.type + '</span>' +
+                    '</span>' +
+                    formatLocation(geo) +
+                '</p>' +
+            '</div>';
+    };
+})();
